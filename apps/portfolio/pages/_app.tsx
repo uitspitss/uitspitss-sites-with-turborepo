6908f231497d19208a4f790f396c2c1172bb5f64
uitspitss <3781626+uitspitss@users.noreply.github.com>
2022-10-07 08:57:11 +0900

    Fix about next/script warnings

--- apps/portfolio/pages/_app.tsx
+++ apps/portfolio/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@fontsource/open-sans/700.css';
 
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 import { ChakraProvider } from '@chakra-ui/react';
 import { appWithTranslation } from 'next-i18next';
 
@@ -21,22 +22,25 @@ const CustomApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   return (
     <>
       <Head>
-        <title>uitspitss&apos;s portfolio</title>
+        <title>uitspitss&apos; portfolio</title>
         {GA_MEASUREMENT_ID && (
           <>
-            <script
-              async
+            <Script
+              id="gtag"
               src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+              onReady={() => {
+                // @ts-expect-error gtag
+                window.dataLayer = window.dataLayer || [];
+                function gtag() {
+                  // @ts-expect-error gtag
+                  window.dataLayer.push(arguments);
+                }
+                // @ts-expect-error gtag
+                gtag('js', new Date());
+                // @ts-expect-error gtag
+                gtag('config', GA_MEASUREMENT_ID);
+              }}
             />
-            <script>
-              {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){window.dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${GA_MEASUREMENT_ID}');
-          `}
-            </script>
           </>
         )}
       </Head>
