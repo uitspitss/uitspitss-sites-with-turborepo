ff029b1ed2c3e85cbe58d80ae5839e2c13cb8ae2
uitspitss <3781626+uitspitss@users.noreply.github.com>
2022-11-05 06:23:49 +0900

    Add PrismaService

---
+++ apps/otoge-bookmark-api/src/prisma/prisma.service.ts
@@ -0,0 +1,19 @@
+import {
+  type INestApplication,
+  Injectable,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdonwHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
