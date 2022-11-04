fbeb9998b8e1ab378ba65a3864139854d152985e
uitspitss <3781626+uitspitss@users.noreply.github.com>
2022-11-05 06:53:31 +0900

    Add repl

--- apps/otoge-bookmark-api/src/main.ts
+++ apps/otoge-bookmark-api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Otoge Bookmark API')
+    .setDescription('Otoge Bookmark API')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
