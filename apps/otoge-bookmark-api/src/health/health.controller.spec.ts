62b48d89a4793bc927857108aabf79e69ea5729b
uitspitss <3781626+uitspitss@users.noreply.github.com>
2022-11-24 01:55:39 +0900

    Fix test

--- apps/otoge-bookmark-api/src/health/health.controller.spec.ts
+++ apps/otoge-bookmark-api/src/health/health.controller.spec.ts
@@ -1,12 +1,17 @@
+import { PrismaModule } from '@/prisma/prisma.module';
+import { TerminusModule } from '@nestjs/terminus';
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
+import { PrismaHealthIndicator } from './indicators/prisma.indicator';
 
 describe('HealthController', () => {
   let controller: HealthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TerminusModule, PrismaModule],
       controllers: [HealthController],
+      providers: [PrismaHealthIndicator],
     }).compile();
 
     controller = module.get<HealthController>(HealthController);
