2013977209f61ef2718692d1352f0b62089e6bc6
uitspitss <3781626+uitspitss@users.noreply.github.com>
2022-11-26 02:09:03 +0900

    Update packages

--- packages/eslint-config-custom/lib/storybook.js
+++ packages/eslint-config-custom/lib/storybook.js
@@ -1,4 +1,12 @@
 module.exports = {
   plugins: ['storybook'],
   extends: ['plugin:storybook/recommended'],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.stories.tsx'],
+      },
+    ],
+  },
 };
