3fc438df57c1744d7000e7b21d8264a246791576
uitspitss <3781626+uitspitss@users.noreply.github.com>
2022-12-01 08:42:08 +0900

    Update

--- packages/eslint-config-custom/presets/react.js
+++ packages/eslint-config-custom/presets/react.js
@@ -1,10 +1,10 @@
 module.exports = {
   extends: [
     '../lib/base.js',
-    '../lib/prettier.js',
     '../lib/typescript.js',
     '../lib/storybook.js',
     '../lib/jest.js',
     '../lib/react.js',
+    '../lib/prettier.js',
   ],
 };
