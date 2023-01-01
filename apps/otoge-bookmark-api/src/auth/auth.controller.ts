2aa94f5ffcca3f7090fee4fb846fab0a07ec906a
uitspitss <3781626+uitspitss@users.noreply.github.com>
2023-01-02 06:15:43 +0900

    Remove auth endpoints

--- apps/otoge-bookmark-api/src/auth/auth.controller.ts
+++ apps/otoge-bookmark-api/src/auth/auth.controller.ts
@@ -1,34 +1,9 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpCode,
-  Post,
-  UseGuards,
-} from '@nestjs/common';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
-import { User } from '@/common/decorators/user.decorator';
-import { JwtAuthGuard } from '@/common/guards/jwt-auth.guard';
-import { UserJwtPayload } from '@/common/interfaces/user-jwt-payload.interface';
+import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
-import { LoginDto } from './dto/login.dto';
-import { LoggedInTokenEntity } from './entities/logged-in-token.entity';
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-
-  @HttpCode(200)
-  @Post('login')
-  @ApiOkResponse({ type: LoggedInTokenEntity })
-  async login(@Body() data: LoginDto) {
-    return this.authService.login(data);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('logout')
-  async logout(@User() user: UserJwtPayload) {
-    this.authService.logout(user.sub);
-  }
 }
